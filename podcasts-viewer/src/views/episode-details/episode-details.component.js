e37e5d03363c145782706ccee466db86b95898f7
Rocío Varela <rocio@toshiba.casa>
2017-07-30 20:14:20 +0200

    Display episode details

--- podcasts-viewer/src/views/episode-details/episode-details.component.js
+++ podcasts-viewer/src/views/episode-details/episode-details.component.js
@@ -4,9 +4,16 @@ import './episode-details.component.css';
 
 class EpisodeDetails extends Component {
     render(){
+        const episodeId = this.props.match.params.episodeId;
+        const episodes = this.props.location.state.episodes;
+        const episode=episodes.filter((item) => {
+            return (item.id===episodeId);
+        })[0];
         return(
             <div>
-               this is episode details
+                <h2> {episode.title} </h2>
+                <p> {episode.summary}</p>
+                <audio src={episode.audioUrl} controls></audio>
             </div>
         );
     }
