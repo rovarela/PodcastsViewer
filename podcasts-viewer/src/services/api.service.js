978d37faa8c18a9df580e4d858e5596f022eace2
Rocío Varela <rocio@toshiba.casa>
2017-07-30 18:59:43 +0200

    Fetch podcasts data from iTunnes

---
+++ podcasts-viewer/src/services/api.service.js
@@ -0,0 +1,5 @@
+import {Fetch} from 'whatwg-fetch';
+
+export function fetchPodcasts() {
+    return fetch('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json');
+}
--- podcasts-viewer/src/views/podcasts/podcasts.component.js
+++ podcasts-viewer/src/views/podcasts/podcasts.component.js
@@ -4,8 +4,40 @@ import { Switch, Route, Link, Redirect } from 'react-router-dom';
 import './podcasts.component.css';
 
 import PodcastDetails from '../podcast-details/podcast-details.component';
+import {fetchPodcasts} from '../../services/api.service'
 
 class Podcasts extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {podcasts_original: [], podcasts_list: []};
+        this.fetchPodcasts = this.fetchPodcasts.bind(this);
+    }
+
+    componentDidMount() {
+        this.fetchPodcasts();  
+    }
+
+    fetchPodcasts(){
+        fetchPodcasts().then(
+                (response) => {
+                    return response.json()
+                }
+        ).then(
+                (json) => {
+                    console.log('parsed json', json);
+                    this.setState({
+                        podcasts_original: json.feed.entry,
+                        podcasts_list: json.feed.entry
+                    });
+                }
+        ).catch(
+                (ex) => {
+                    console.log('parsing failed', ex);
+        }
+        );
+    }
+
 
 
     renderPodcastList() {
